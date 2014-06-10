ba2565bed20883f35b20673f824072b914ee7e5f
Richard Forrest <richard.a.forrest@gmail.com>
2014-06-10 16:24:25 +0100

    Added unit test for as extension nullable types

---
+++ Utilities.Test/Extensions/ObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Utilities.Extensions;
+using Xunit;
+
+namespace Utilities.Test.Extensions
+{
+	public class ObjectExtensions
+	{
+		[Fact]
+		public void As_should_cast_empty_string_to_nullable_int()
+		{
+			var source = "";
+
+			var actual = source.As<int?>();
+
+			Assert.False(actual.HasValue);
+		}
+	}
+}
